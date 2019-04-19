5affca907bf519193b16680af1e5cb7b476f3e2b
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-04-19 11:54:09 +0200

    Add routes

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import * as serviceWorker from './serviceWorker';
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
 import { Provider } from 'react-redux';
 import store from './store';
 import DevTools from './DevTools';
 import { getCountries } from './actions/actions-countries';
+import routes from './routes';
 
 render(
     <Provider store={store}>
         <div>
-            <h1>Inicjalizacja projektu</h1>
+            <Router history={hashHistory} routes={routes}/>                
             <DevTools />
         </div>
     </Provider>,
