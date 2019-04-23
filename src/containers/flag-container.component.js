7e6de0540b1e3f3849959827e4a1aaedd2cac53a
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-04-23 13:32:20 +0200

    Add search country

--- src/containers/flag-container.component.js
+++ src/containers/flag-container.component.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import CountryFlagList from '../presentational/flag-list.component';
-import { getCountries } from '../actions/actions-countries';
+import { getCountries, searchCountries } from '../actions/actions-countries';
+
 
 class CountryFlagContainer extends Component {
   constructor(props) {
@@ -9,21 +10,29 @@ class CountryFlagContainer extends Component {
   }
 
   componentDidMount() {
-    this.props.dispatch(getCountries());
+    this.props.dispatch(searchCountries(''));
+  }
+
+  search(event) {
+    this.props.dispatch(searchCountries(event.target.value));
   }
 
   render() {
     return (
       <div>
-        <CountryFlagList countries={this.props.countries} />
+        <div className="search text-center">
+          <input type="text" onChange={this.search.bind(this)} />
+        </div>
+        <CountryFlagList countries={this.props.visibleCountries} />
       </div>
     );
   }
 }
 
-const mapStateToProps = function(store) {
+const mapStateToProps = function (store) {
   return {
-    countries: store.countriesReducer.countries
+    countries: store.countriesReducer.countries,
+    visibleCountries: store.countriesReducer.visibleCountries
   };
 };
 
