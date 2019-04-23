ff105a34e571db8c6c590751b0c27533eb026e15
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-04-23 17:24:23 +0200

    Updates

---
+++ precache-manifest.d6640e06e6523fd2526b6f10cda84c61.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "dcb6a77eb77f9a0dae76",
+    "url": "/static/css/main.691aefe8.chunk.css"
+  },
+  {
+    "revision": "dcb6a77eb77f9a0dae76",
+    "url": "/static/js/main.22138ae5.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1ea9956f3d0699918033",
+    "url": "/static/css/2.b5c6462b.chunk.css"
+  },
+  {
+    "revision": "1ea9956f3d0699918033",
+    "url": "/static/js/2.f84786a9.chunk.js"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "411711aa5d93083a3b45d3a1c2494780",
+    "url": "/index.html"
+  }
+];
